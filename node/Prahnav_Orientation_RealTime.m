c76a3b12154acb3d2c11908f1cd0d7221ed51b47
Prahnav <Prahnav@dyn1201-227.wlan.ic.ac.uk>
2017-02-28 15:24:21 +0000

    Working DEMO FILE

--- node/Prahnav_Orientation_RealTime.m
+++ node/Prahnav_Orientation_RealTime.m
@@ -1,98 +1,95 @@
 function Prahnav_Orientation_RealTime(input_file)
 %-------------------------------------------------------------------------%
-% Housekeeping
-addpath('ximu_matlab_library');	% include x-IMU MATLAB library
-addpath('quaternion_library');	% include quatenrion library
-
+addpath('ximu_matlab_library');	    % include x-IMU MATLAB library
+addpath('quaternion_library');	    % include quatenrion library
+load neural_network.mat
 %-------------------------------------------------------------------------%
 % Variable Initializations
 
 %Sample Period
-samplePeriod = 1/64;
-
-%Check
-check = 0;
-prev_acc = zeros(1,3);
+samplePeriod = 1/53;                
 
 % Origin
-origin = [0,0,0];
+origin = [0,0,0];                   
 
-% Thrshold
-threshold = 0.1;
+% Rotation and Angles Matrix 
+R = zeros(3,3); E = zeros(3,3,1);   
 
-% Rotation Matrix describing Sensor Relative to Earth
-R = zeros(3,3,1);     
+% Gyroscope Threshold
+thresh = 12.5;
 
-  ahrs = MahonyAHRS('SamplePeriod', samplePeriod, 'Kp', 1);
-%ahrs = MadgwickAHRS('SamplePeriod', samplePeriod, 'Beta', 0.1);
+% Process Data through AHRS Algorithm (Calculate Orientation)
+ahrs = MahonyAHRS('SamplePeriod', samplePeriod, 'Kp', 1);
+%   ahrs = MadgwickAHRS('SamplePeriod', samplePeriod, 'Beta', 0.1);
+
+% Neural Net Threshold
+net_threshold = 1.5;
+
 %-------------------------------------------------------------------------%
 % Real Time Implementation
-fig1 = figure
-while(1)
 
-    % File Reading
-    data = csvread(input_file,1);
+fig1 = figure
+
+while(1)
+    % File Reading 
+    data = csvread(input_file,1); 
     
     % Filter Duplicates
-    [~,idx]=unique(data,'rows','first');
-    out=data(idx,:);
+    [~,idx]=unique(data,'rows','first');out=data(idx,:);
     
-    % Split File Data into Accelerometer, Gyroscope and Magnetometer
-    [time,acc,mag,gyr]=splitData(out);
-    
-%     if check == 0
-%         prev_acc = acc(end,:);
-%         check = check + 1;
-%     else
-%         if abs(acc(end,1)-prev_acc(end,1)) < threshold
-%             acc(end,1) = prev_acc(end,1);
-%         end
-%         if abs(acc(end,2)-prev_acc(end,2)) < threshold
-%             acc(end,2) = prev_acc(end,2);
-%         end
-%         if abs(acc(end,3)-prev_acc(end,3)) < threshold
-%             acc(end,3) = prev_acc(end,3);
-%         end
-%         prev_acc = acc(end,:);     
-%     end
-    
-    
+    % Split Data
+    [time,acc,mag,gyr]=splitData(data);      
+    input_net = [acc(end,:), gyr(end,:)]';
     clear csv_data
-   
-    % Core Processing
+    
+    if abs(gyr(end,1)) < thresh
+        gyr(end,1) = 0;
+    end
+    if abs(gyr(end,2)) < thresh
+        gyr(end,2) = 0;
+    end
+    if abs(gyr(end,3)) < thresh
+        gyr(end,3) = 0;
+    end
+    
     %---------------------------------------------------------------------%
-    % Process Data through AHRS Algorithm (Calculate Orientation)
-
     %Update AHRS and ensure Gyroscope Units are in Radians
     ahrs.UpdateIMU(gyr(end,:) * (pi/180), acc(end,:));	
     
     % Transpose because AHRS provides Earth relative to sensor
-    R(:,:,end) = quatern2rotMat(ahrs.Quaternion)';
+    R(:,:) = quatern2rotMat(ahrs.Quaternion)';
 
-% Figures for Result
- 
-%-------------------------------------------------------------------------%
-        ux = R(1,1,end);
-        vx = R(2,1,end);
-        wx = R(3,1,end);
-        uy = R(1,2,end);
-        vy = R(2,2,end);
-        wy = R(3,2,end);
-        uz = R(1,3,end);
-        vz = R(2,3,end);
-        wz = R(3,3,end);
-        
-        X = [origin; [ux,vx,wx]];
-        Y = [origin; [uy,vy,wy]];
-        Z = [origin; [uz,vz,wz]];
-        
-        view(3);
-        plot3(X(:,1),X(:,2),X(:,3),Y(:,1),Y(:,2),Y(:,3),Z(:,1),Z(:,2),Z(:,3));
-        grid on;
-        axis([-1,1,-1,1,-1,1]);
-        pause(0.0001);
+%     E = rotm2eul(R);
+%     dlmwrite ('outputData.csv', E, '-append');
 
+    %---------------------------------------------------------------------%
+    % Figure for Visualizing Orientation
+    ux = R(1,1,end); vx = R(2,1,end); wx = R(3,1,end);
+    uy = R(1,2,end); vy = R(2,2,end); wy = R(3,2,end);
+    uz = R(1,3,end); vz = R(2,3,end); wz = R(3,3,end);    
+   
+    X = [origin; [ux,vx,wx]]; 
+    Y = [origin; [uy,vy,wy]]; 
+    Z = [origin; [uz,vz,wz]];
+    
+    
+    %-------------------%
+    V = [origin; [ux + uy + uz, vx + vy + vz, wx + wy + wz]];
+    
+    %-------------------%
+    
+    view(3); grid on; axis([-1,1,-1,1,-1,1]); pause(0.0001);
+   plot3(X(:,1),X(:,2),X(:,3),Y(:,1),Y(:,2),Y(:,3),Z(:,1),Z(:,2),Z(:,3));
+%     plot3(V(:,1),V(:,2),V(:,3));
+    net_output = networkThird(input_net)
+    if net_output < net_threshold
+       text_string = 'Good';
+    else
+       text_string = 'Bad';
+    end
+    str=sprintf('User Posture = %s', text_string);
+    title(str);
 
 end
-
 %% End of script
+
