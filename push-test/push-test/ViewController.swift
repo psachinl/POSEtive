3fd5e1e176a821728714015c0af3dae3d7d273ba
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-03-07 18:38:46 +0000

    Initial push notification via APNs testing

---
+++ push-test/push-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  push-test
+//
+//  Created by Sachin Leelasena on 07/03/2017.
+//  Copyright © 2017 Sachin Leelasena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
