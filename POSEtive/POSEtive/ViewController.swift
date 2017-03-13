c5842283420fd4cedbe9e9e861cbce3c9df57dc6
Sachin Leelasena <psachinl@users.noreply.github.com>
2017-03-13 17:15:43 +0000

    Added app files and icon

---
+++ POSEtive/POSEtive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POSEtive
+//
+//  Created by Sachin Leelasena on 13/03/2017.
+//  Copyright © 2017 POSEtive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
